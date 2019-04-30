9df0bf16d0175ead1433b73a217ccde911ae7ef0
prahyas <xrestha17@gmail.com>
2019-04-30 13:54:56 +0545

    Supplychain database added

---
+++ src/supplyc.EntityFrameworkCore/EntityFrameworkCore/supplycDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using supplyc.Authorization.Roles;
+using supplyc.Authorization.Users;
+using supplyc.MultiTenancy;
+
+namespace supplyc.EntityFrameworkCore
+{
+    public class supplycDbContext : AbpZeroDbContext<Tenant, Role, User, supplycDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public supplycDbContext(DbContextOptions<supplycDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
